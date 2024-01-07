47819b4294ccf66e89a0a727566966195f03a8af
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-01-07 17:02:10 +0100

    Added slash to reliably copy whole directory

--- aerc.nix
+++ aerc.nix
@@ -8,7 +8,7 @@
   home.activation = {
     aercConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
       $DRY_RUN_CMD rm -rf ${config.home.homeDirectory}/.config/aerc/
-      $DRY_RUN_CMD cp -r ${builtins.toPath ./dotfiles/aerc} ${config.home.homeDirectory}/.config/
+      $DRY_RUN_CMD cp -r ${builtins.toPath ./dotfiles/aerc}/ ${config.home.homeDirectory}/.config/
       $DRY_RUN_CMD chmod 600 ${config.home.homeDirectory}/.config/aerc/accounts.conf
     '';
   };

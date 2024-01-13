240b284671cda339513ede0357fda20bec972a3c
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-01-13 20:02:43 +0100

    Added espanso service

--- espanso.nix
+++ espanso.nix
@@ -9,4 +9,19 @@
     source = ./dotfiles/espanso;
     recursive = true;
   };
+
+  systemd.user.services.espanso-unmanaged = {
+    Unit = {
+      Description = "Start espanso for the user session";
+    };
+    Service = {
+      ExecStart = "${pkgs.writeShellScript "espanso-unmanaged" ''
+      #!/usr/bin/env bash
+      ${pkgs.espanso}/bin/espanso service start --unmanaged
+    ''}";
+    };
+    Install = {
+      WantedBy = [ "default.target" "graphical.target" "multi-user.target" ];
+    };
+  };
 }

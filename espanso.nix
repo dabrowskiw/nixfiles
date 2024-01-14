895421b30c7e5f4947eb830d015c0f0c1b13d68e
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-01-14 14:18:01 +0100

    Start espanso service on i3 startup

--- espanso.nix
+++ espanso.nix
@@ -9,19 +9,4 @@
     source = ./dotfiles/espanso;
     recursive = true;
   };
-
-  systemd.user.services.espanso-unmanaged = {
-    Unit = {
-      Description = "Start espanso for the user session";
-    };
-    Service = {
-      ExecStart = "${pkgs.writeShellScript "espanso-unmanaged" ''
-      #!/usr/bin/env bash
-      ${pkgs.espanso}/bin/espanso service start --unmanaged
-    ''}";
-    };
-    Install = {
-      WantedBy = [ "default.target" "graphical.target" "multi-user.target" ];
-    };
-  };
 }

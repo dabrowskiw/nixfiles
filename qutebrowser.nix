2b076378e01ad9cac14fe5ed273ec8ab9ffbbf3b
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-01-14 11:55:08 +0100

    Use qutebrowser as default html handler

--- qutebrowser.nix
+++ qutebrowser.nix
@@ -5,6 +5,19 @@
     pkgs.qutebrowser
   ];
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+    };
+  };
+
+  home.sessionVariables.DEFAULT_BROWSER = "${pkgs.qutebrowser}/bin/qutebrowser";
+
   home.activation = {
     qutebrowserConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
       $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.config/qutebrowser/

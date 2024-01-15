c91a2068aee10d8ad7b211f269a1aeeaf60c2374
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-01-15 18:39:12 +0100

    i3 shortcut to disable touchpad

---
+++ dotfiles/toggleTouchpad.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+set -l tid (xinput | grep "Touchpad" | cut -d "=" -f 2 | sed -e 's/\\s.*//g')
+set -l tstate (xinput list-props $tid | grep "Device Enabled" | cut -d ":" -f 2 | sed -e 's/\\s*//g')
+if test $tstate -eq 1
+  xinput set-prop $tid "Device Enabled" 0
+else
+  xinput set-prop $tid "Device Enabled" 1
+end
--- i3.nix
+++ i3.nix
@@ -7,5 +7,6 @@
   };
 
   home.file.".config/i3/startPcloud.fish".source = ./dotfiles/startPcloud.fish;
+  home.file.".config/i3/toggleTouchpad.fish".source = ./dotfiles/toggleTouchpad.fish;
 }
 

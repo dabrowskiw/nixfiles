19cfd9ae8ee4f08c45ee62c52f9b78c274fbd9aa
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-01-27 10:47:16 +0100

    Fixed .gnupg permissions

--- gpg.nix
+++ gpg.nix
@@ -17,6 +17,8 @@
     gpg = lib.hm.dag.entryAfter ["writeBoundary"] ''
       $DRY_RUN_CMD rm -rf ${config.home.homeDirectory}/.gnupg
       $DRY_RUN_CMD cp -r ${builtins.toPath ./secrets/.gnupg} ${config.home.homeDirectory}/
+      $DRY_RUN_CMD chmod -R 700 ${config.home.homeDirectory}/.gnupg
+      $DRY_RUN_CMD chmod 600 ${config.home.homeDirectory}/.gnupg/*
     '';
   };
 }

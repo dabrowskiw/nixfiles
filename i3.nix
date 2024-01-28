c9ab0a85a703c4879b516a8eccfac2a41056439d
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-01-28 12:50:34 +0100

    Shortcut for playground sessions and other lutris stuff

--- i3.nix
+++ i3.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 { 
   home.file.".config/i3" = {
@@ -8,5 +8,10 @@
 
   home.file.".config/i3/startPcloud.fish".source = ./dotfiles/startPcloud.fish;
   home.file.".config/i3/toggleTouchpad.fish".source = ./dotfiles/toggleTouchpad.fish;
+  home.file.".config/i3/runlutrisgame".source = pkgs.writeShellScript "runlutrisgame" ''
+    gameid=`lutris --list-games | grep  "$1" | head -n 1 | cut -d "|" -f 1 | sed -e 's/ //g'`
+    lutriscmd="env LUTRIS_SKIP_INIT=1 lutris lutris:rungameid/$gameid"
+    $lutriscmd
+  '';
 }
 

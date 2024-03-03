e1f02c7cf5bc753d11986d4c493bcc881b50bb91
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-03-03 11:10:26 +0100

    Temporarily removed overwriting .gnupg until I have figured out the permission error this causes...

--- gpg.nix
+++ gpg.nix
@@ -16,13 +16,13 @@
     pinentryFlavor = "qt";
   };
 
-  home.activation = {
-    gpg = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.gnupg || true
-      $DRY_RUN_CMD cp -rn ${builtins.toPath ./secrets/.gnupg} ${config.home.homeDirectory}/ || true 
-      $DRY_RUN_CMD chmod -R 700 ${config.home.homeDirectory}/.gnupg || true
-      $DRY_RUN_CMD chmod 600 ${config.home.homeDirectory}/.gnupg/* || true
-    '';
-  };
+#  home.activation = {
+#    gpg = lib.hm.dag.entryAfter ["writeBoundary"] ''
+#      $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.gnupg || true
+#      $DRY_RUN_CMD cp -rn ${builtins.toPath ./secrets/.gnupg} ${config.home.homeDirectory}/ || true 
+#      $DRY_RUN_CMD chmod -R 700 ${config.home.homeDirectory}/.gnupg || true
+#      $DRY_RUN_CMD chmod 600 ${config.home.homeDirectory}/.gnupg/* || true
+#    '';
+#  };
 }
 

bc8d39534f244f2ce932df7a37285e592055de27
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-03-22 14:11:28 +0100

    Removed pytest

--- dotfiles/lvim/config.lua
+++ dotfiles/lvim/config.lua
@@ -62,24 +62,6 @@ lvim.plugins = {
     lazy = false
   },
   {
-    "nvim-neotest/neotest",
-    dependencies = "nvim-neotest/neotest-python",
-    config = function() 
-      require("neotest").setup({
-        adapters = {
-          require("neotest-python")({
-            dap = {
-              justMyCode = false,
-              console = "integratedTerminal",
-            },
-            args = { "--log-level", "DEBUG", "--quiet" },
-            runner = "pytest",
-          })
-        }
-      })
-    end
-  },
-  {
     "martineausimon/nvim-lilypond-suite",
     ft = { "lilypond", "tex", "texinfo" },
     config = function()

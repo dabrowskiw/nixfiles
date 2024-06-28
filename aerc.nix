46e0e2a15d22d326c3ddedbdf03422dc9afb2c38
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-06-28 09:50:27 +0200

    Added proper reply filter for html-only mails

--- aerc.nix
+++ aerc.nix
@@ -13,6 +13,8 @@
       $DRY_RUN_CMD rm -rf ${config.home.homeDirectory}/.config/aerc/
       $DRY_RUN_CMD cp -r ${builtins.toPath ./dotfiles/aerc}/ ${config.home.homeDirectory}/.config/
       $DRY_RUN_CMD chmod 600 ${config.home.homeDirectory}/.config/aerc/accounts.conf
+      $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.config/aerc/filters
+      $DRY_RUN_CMD ln -s ${pkgs.aerc}/libexec/aerc/filters/html ${config.home.homeDirectory}/.config/aerc/filters/html
     '';
   };
 }

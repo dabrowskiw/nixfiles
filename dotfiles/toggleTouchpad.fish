f876a2e27b137f2767530bf98d831dda89cd3e1b
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-07-11 13:36:50 +0200

    touchpad toggle

--- dotfiles/toggleTouchpad.fish
+++ dotfiles/toggleTouchpad.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set -l tid (xinput | grep "Touchpad" | cut -d "=" -f 2 | sed -e 's/\\s.*//g')
+set -l tid (xinput | grep -i "touchpad" | cut -d "=" -f 2 | sed -e 's/\\s.*//g')
 set -l tstate (xinput list-props $tid | grep "Device Enabled" | cut -d ":" -f 2 | sed -e 's/\\s*//g')
 if test $tstate -eq 1
   xinput set-prop $tid "Device Enabled" 0

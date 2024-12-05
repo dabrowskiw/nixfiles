c037c4ec2583955104e1e9d07c18e2996c15e5f6
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-12-05 20:54:07 +0100

    Cleanup

--- dotfiles/fzffolder.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-folder=$(find /home/wojtek/Maildir/htw/ -type d | grep -vE "/tmp$|/cur$|/new$" | fzf)
-
-
-echo "push 'c$folder<enter>'"
--- dotfiles/qutebrowser/config.py
+++
@@ -1,7 +0,0 @@
-config.load_autoconfig()
-config.bind('q', 'tab-close')
-config.bind('<Ctrl-n>', 'tab-next')
-config.bind('<Ctrl-p>', 'tab-prev')
-
-config.set("colors.webpage.bg", "#ffffffff")
-

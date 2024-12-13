8297087e1ae32052b9ac83e5c76f084ae3ab0083
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-12-13 16:42:52 +0100

    Cleanup

--- dotfiles/maptablet.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-set elements (xsetwacom --list devices)
-for e in $elements
-  set id (string trim (string split "type: " (string split "id: " $e)[2])[1]) 
-  xsetwacom --set "$id" MapToOutput DP-3-3
-end

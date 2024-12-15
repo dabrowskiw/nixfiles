5245be1f5651fad4881fa4291906c898db281a9a
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-12-15 16:06:11 +0100

    Moved from difftastic to delta

--- git.nix
+++ git.nix
@@ -3,10 +3,13 @@
 {
   programs.git = {
     enable = true;
-    difftastic.enable = true;
-    difftastic.color = "always";
-    difftastic.display = "side-by-side-show-both";
     userEmail = "piotr.dabrowski@htw-berlin.de";
     userName = "Piotr Wojciech Dabrowski";
+    delta = {
+      enable = true;
+      options = {
+        "side-by-side" = true;
+      };
+    };
   };
 }

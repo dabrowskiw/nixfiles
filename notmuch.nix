57a68abca3d400c2821441b7305b21cd15244746
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-12-15 16:07:15 +0100

    Moved notmuch config to nix

--- notmuch.nix
+++ notmuch.nix
@@ -1,9 +1,28 @@
 { pkgs, ... }:
 
+let
+  notmuchconfig = pkgs.writeTextFile {
+    name = "notmuchconfig";
+    destination = "/share/notmuchconfig";
+    text = ''
+      [database]
+      path=/home/wojtek/data/mail
+      [user]
+      name=Piotr Wojciech Dabrowski
+      primary_email=piotr.dabrowski@posteo.de
+      other_email=piotr.dabrowski@htw-berlin.de;dabrows@htw-berlin.de;piotr.dabrowski@posteo.net;
+      [new]
+      tags=inbox
+      [search]
+      [maildir]
+    '';
+  };  
+in
 {
   home.packages = [
     pkgs.notmuch
+    notmuchconfig
   ];
 
-  home.file.".notmuch-config".source = ./dotfiles/.notmuch-config;
+  home.file.".notmuch-config".source = ${notmuchconfig}/share/notmuchconfig;
 }

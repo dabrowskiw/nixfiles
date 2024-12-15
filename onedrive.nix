b0ae6f70ac6e452b7feabb255eb19a3ec678ecc5
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2024-12-15 16:19:20 +0100

    Commented out onedrive secrets

--- onedrive.nix
+++ onedrive.nix
@@ -7,8 +7,8 @@
 
   home.activation = {
     onedriveConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.config/onedrive/
-      $DRY_RUN_CMD cp -ru ${builtins.toPath ./secrets/onedrive}/* ${config.home.homeDirectory}/.config/onedrive/
+#      $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.config/onedrive/
+#      $DRY_RUN_CMD cp -ru ${builtins.toPath ./secrets/onedrive}/* ${config.home.homeDirectory}/.config/onedrive/
     '';
  };
 }

0a18c0a063bb8adfbcab058f656dd9c2c5ffca04
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2025-05-13 21:37:22 +0200

    Tmux popup for khal autocomplete

--- nixfiles/tools/tmux.nix
+++ nixfiles/tools/tmux.nix
@@ -1,12 +1,26 @@
 { pkgs, ... }:
 
+let 
+  formatmail = pkgs.writeTextFile {
+    name = "khal_fzf";
+    destination = "/bin/khal_fzf";
+    executable = true;
+    text = (builtins.readFile ../../scripts/khal_fzf);
+  };
+in
 {
+  home.packages = [
+    formatmail
+  ];
 
   programs.tmux = {
     enable = true;
     keyMode = "vi";
     secureSocket = true;
     escapeTime = 0;
+    extraConfig = ''
+      bind f run-shell "khal_fzf; tmux set-buffer \"$(xsel -o --clipboard)\"; tmux paste-buffer;"
+    '';
     plugins = with pkgs.tmuxPlugins; [
       onedark-theme
       tmux-fzf

47c8e723f1c9486d3d9cdf8fcd98c8d5bb688c20
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2026-01-27 18:06:24 +0100

    Upgrade to 25.11

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     # NixOS official package source, using the nixos-24.05 branch here
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
